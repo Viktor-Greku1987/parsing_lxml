1d2e408b7b782f482b8d194277d2acb1b7f4ce6d
unknown <Viktor-Greku1987>
2023-04-06 18:20:07 +0300

    first_commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 link  = 'https://browser-info.ru/'
 responce = requests.get(link).text
 #print(responce)
@@ -9,8 +10,20 @@ main = soup.find('div', id="main")
 center = main.find('div', id="center")
 tool = center.find('div', id="tool_padding")
 ch_js = tool.find('div', id="javascript_check")
-print(ch_js)
+
+off  = ch_js.find_all('span')[1].text
+result_js = f'javascript: {off}'
+print('off = ', result_js)
+print(result_js)
 
 
-#check_js = block.find('div', id="javascript_check")
-#print(check_js)
+div_flash = tool.find('div', id="flash_version")
+#div_user = tool.find('div', id="user_agent" )
+#user_ag = div_user.find_all('span', attrs= {"class":"option_title"})
+
+flash = div_flash.find_all('span')[1].text
+result_flash = f'flash: {flash}'
+
+check_user = tool.find('div', id = "user_agent").text
+print("check_user: = ", check_user)
+
